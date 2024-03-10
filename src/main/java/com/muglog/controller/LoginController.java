87b4cf9915fb4d1ae66811f5d56c3770471c94c6
yooputer <yooputer@gmail.com>
2024-03-10 12:45:07 +0900

    [로그인] 구글로그인시 임시 jwt 대신 userId를 담은 JWT 토큰 반환하도록 수정

--- src/main/java/com/muglog/controller/LoginController.java
+++ src/main/java/com/muglog/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.muglog.controller;
 
-import com.muglog.dto.LoginRequest;
+import com.muglog.dto.login.LoginRequest;
+import com.muglog.utils.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,32 +14,32 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping("/googleLogin")
     public ResponseEntity<?> googleLogin(@RequestBody LoginRequest loginRequest){
-        // 프론트에서 받은 assessToken을 사용해 구글 유저 정보 조회
-        WebClient client = WebClient.create("https://www.googleapis.com");
-        Map<String, Object> response = client
-                .get()
-                .uri(
-                        uriBuilder ->
-                                uriBuilder
-                        .path("/oauth2/v1/userinfo")
-                        .queryParam("access_token", loginRequest.getAccessToken())
-                        .build()
-                )
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(Map.class)
-                .block();
-
         try{
+            // 프론트에서 받은 assessToken을 사용해 구글 유저 정보 조회
+            WebClient client = WebClient.create("https://www.googleapis.com");
+            Map<String, Object> response = client
+                    .get()
+                    .uri(
+                            uriBuilder ->
+                                    uriBuilder
+                            .path("/oauth2/v1/userinfo")
+                            .queryParam("access_token", loginRequest.getAccessToken())
+                            .build()
+                    )
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(Map.class)
+                    .block();
+
             // 반환값에서 email, name 추출
             String email = response.get("email").toString();
             String name = response.get("name").toString();
-            String jwt = "임시 jwt"; // TODO jwt 토큰 생성
-
-            // TODO member_info 테이블에 upsert
+            Long userId = 1L; // TODO db에서 email, socialLoginType으로 유저아이디 조회하기
+            String jwt = JwtUtil.createJwtToken(userId);
 
             Map<String,Object> resMap = new HashMap<>();
             resMap.put("jwt", jwt);
@@ -45,6 +48,7 @@ public class LoginController {
 
             return new ResponseEntity<>(resMap, new HttpHeaders(), HttpStatus.OK);
         }catch (Exception e){
+            logger.error("Exception", e);
             return ResponseEntity.badRequest().build();
         }
     }
